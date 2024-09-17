6c7bfa769c7c51efea7c9b21d8f88ebf56a244a7
Aytuu1 <Aytuuc1@gmail.com>
2024-09-17 14:18:52 +0300

    Typescript

--- typescript/TypeScriptTutorial.js
+++ typescript/TypeScriptTutorial.js
@@ -30,3 +30,110 @@ anyValue = "Ben bir stringim";
 console.log(anyValue);
 anyValue = true;
 console.log(anyValue);
+// 8-) Union Type: Birden fazla tür eklemek istediğimizde
+var unionTypeValue;
+unionTypeValue = "ts ögreniyorum";
+console.log(unionTypeValue);
+unionTypeValue = 44;
+console.log(unionTypeValue);
+//9-) Void : Fonksiyon dönüş değeri olmasın.
+function normalFnc() {
+    console.log("Normal Fonksiyon");
+}
+normalFnc();
+// 10-) Enum: Sabit listeler
+var WeekDay;
+(function (WeekDay) {
+    WeekDay[WeekDay["Monday"] = 0] = "Monday";
+    WeekDay[WeekDay["Tuesday"] = 1] = "Tuesday";
+    WeekDay[WeekDay["Wednesday"] = 2] = "Wednesday";
+    WeekDay[WeekDay["Thursday"] = 3] = "Thursday";
+    WeekDay[WeekDay["Friday"] = 4] = "Friday";
+    WeekDay[WeekDay["Saturday"] = 5] = "Saturday";
+    WeekDay[WeekDay["Sunday"] = 6] = "Sunday";
+})(WeekDay || (WeekDay = {}));
+var enumValue = WeekDay.Monday;
+console.log("Enum value:" + enumValue);
+// 11-) Never: Hiçbir değerin olmayacağını gösterir.
+function error(message) {
+    throw new Error(message);
+}
+// 12-) Değer Döndüren Fonksiyon
+function sum(data1, data2) {
+    return data1 + data2;
+}
+var data = sum(3, 4);
+console.log(data);
+function sumRest() {
+    var sayilar = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        sayilar[_i] = arguments[_i];
+    }
+    return sayilar.reduce(function (toplam, sayi) { return toplam + sayi; }, 0);
+}
+var resultRest = sumRest(1, 2, 3, 4);
+console.log(resultRest);
+// 13-) Conditional
+var numValue = 15;
+var result = (numValue > 0) ? "Positive" : "Negative";
+console.log(result);
+// 14-CLASS
+// class Computer{
+//    //Veriable
+//    public cpu:number;
+//    private mainboard:string;
+//    protected isNewVersion:boolean;
+//    // constructor
+//    constructor(cpu:number,mainboard:string,isNewVersion:boolean){
+//       this.cpu=cpu;
+//       this.mainboard=mainboard;
+//       this.isNewVersion=isNewVersion;
+//    }
+//    computerAllInformation():void{
+//       console.log(`CPU:${this.cpu}, Mainboard:${this.mainboard}, Version:${this.isNewVersion}`);
+//    }
+//    // Getter and Setter
+//    public get getCpu(){
+//       return this.cpu;
+//    }
+//    public set setCpu(cpu:number){
+//        this.cpu=cpu;
+//    }
+//    public get getMainBoard(){
+//       return this.mainboard;
+//    }
+//    public set setMainBoard(mainboard:string){
+//        this.mainboard=mainboard;
+//    }
+//    public get getIsNewVersion(){
+//       return this.isNewVersion;
+//    }
+//    public set setIsNewVersion(isnewVersion:boolean){
+//        this.isNewVersion=this.isNewVersion;
+//    }
+// }
+// const computerInstance = new Computer(5,"mainboardxkl",true);
+// console.log(computerInstance.computerAllInformation());
+// 15-) Computer2 : GENERICS YAPILAR
+// class Computer2<T>{
+//    //Veriable
+//    public cpu:T;
+//    public mainboard:T;
+//    public isNewVersion:T; 
+// }
+// const computerValue = new Computer2<string>();
+// computerValue.cpu="generics CPU 4.5GHZ";
+// computerValue.mainboard="generics Mainboard 4.5GHZ";
+// computerValue.isNewVersion="generics 14 Gen";
+// console.log(computerValue.cpu);
+// console.log(computerValue.mainboard);
+// console.log(computerValue.isNewVersion);
+// Abstract 
+var Car = /** @class */ (function () {
+    function Car(carprice, carversion, carmodel) {
+        this.carPrice = carprice;
+        this.carVersion = carversion;
+        this.carModel = carmodel;
+    }
+    return Car;
+}());
